11f7533c2adf119cae1d02660dcf2c90aa674f36
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-10-20 01:21:07 -0500

    docs: add API Documentation for create dish

--- src/main/java/com/prgama/foodcourt_microservice/infrastructure/input/rest/DishRestControllerAdapter.java
+++ src/main/java/com/prgama/foodcourt_microservice/infrastructure/input/rest/DishRestControllerAdapter.java
@@ -4,6 +4,11 @@ import com.prgama.foodcourt_microservice.application.dto.request.CreateDishReque
 import com.prgama.foodcourt_microservice.application.dto.response.ControllerResponse;
 import com.prgama.foodcourt_microservice.application.handler.IDishHandler;
 import com.prgama.foodcourt_microservice.infrastructure.constants.ControllerConstants;
+import com.prgama.foodcourt_microservice.infrastructure.constants.DocumentationConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +26,24 @@ import java.time.LocalDateTime;
 public class DishRestControllerAdapter {
     private final IDishHandler dishHandler;
 
+    @Operation(summary = DocumentationConstants.CREATE_DISH_SUMMARY,
+            tags = {DocumentationConstants.RESTAURANT_TAG, DocumentationConstants.DISH_TAG},
+            description = DocumentationConstants.CREATE_DISH_DESCRIPTION
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = DocumentationConstants.CREATED_STATUS_CODE,
+                    description = DocumentationConstants.CREATED_RESPONSE_CODE_DESCRIPTION_DISH,
+                    content = @Content),
+            @ApiResponse(responseCode = DocumentationConstants.BAD_REQUEST_STATUS_CODE,
+                    description = DocumentationConstants.BAD_REQUEST_RESPONSE_CODE_DESCRIPTION_DISH,
+                    content = @Content),
+            @ApiResponse(responseCode = DocumentationConstants.NOT_FOUND_STATUS_CODE,
+                    description = DocumentationConstants.NOT_FOUND_RESPONSE_CODE_DESCRIPTION_DISH,
+                    content = @Content),
+            @ApiResponse(responseCode = DocumentationConstants.CONFLICT_STATUS_CODE,
+                    description = DocumentationConstants.CONFLICT_RESPONSE_CODE_DESCRIPTION_DISH,
+                    content = @Content),
+    })
     @PostMapping("/create")
     public ResponseEntity<ControllerResponse> createDish(@Valid @RequestBody CreateDishRequest request) {
         dishHandler.createDish(request);
